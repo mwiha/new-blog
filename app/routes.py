6b981cbc17ad310117b26d67e79464d05deec31b
kebasoh <ongatikebasoba@gmail.com>
2020-01-20 18:46:24 +0300

    add requirements.txt

--- app/routes.py
+++ app/routes.py
@@ -8,20 +8,20 @@ from app.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
 from .models import User, Post, Comment
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_mail import Message
-from app import request
+# from app import request
 
 @app.route("/")
 @app.route("/home")
 def home():
     # page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate( per_page=5)
     return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    quote =request.get_quote()
-    return render_template('about.html', title='About', quote=quote)
+    # quote =request.get_quote()
+    return render_template('about.html', title='About')
 
 
 @app.route("/register", methods=['GET', 'POST'])
