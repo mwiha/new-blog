4f3099f956f8a6a8ba3e2cd0a3261775a1e93214
kebasoh <ongatikebasoba@gmail.com>
2020-01-20 16:55:51 +0300

    final commit

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server
 from app.models import *
 # app = create_app('production')
-app = create_app('production')
+app = create_app('development')
 manager = Manager(app)
 migrate = Migrate(app,db)
 manager.add_command('server', Server)
