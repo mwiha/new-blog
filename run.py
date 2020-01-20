9f08f81efdca64e3398180a463674f42964e9264
kebasoh <ongatikebasoba@gmail.com>
2020-01-20 16:59:02 +0300

    add requirements.txt

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from app import create_app, db
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server
 from app.models import *
-# app = create_app('production')
-app = create_app('development')
+app = create_app('production')
+#app = create_app('development')
 manager = Manager(app)
 migrate = Migrate(app,db)
 manager.add_command('server', Server)
