bb1967f283ad07a893527c5a2d35be006588034a
kebasoh <ongatikebasoba@gmail.com>
2020-01-20 15:36:41 +0300

    edited the added the random quotes

--- app/routes.py
+++ app/routes.py
@@ -13,8 +13,8 @@ from app import request
 @app.route("/")
 @app.route("/home")
 def home():
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    # page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=5)
     return render_template('home.html', posts=posts)
 
 
