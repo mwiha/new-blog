8101667983ce327cbc8d23fc1daa950a171f6e97
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-02-21 12:05:47 +0300

    Package the app

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kebaso/Desktop/BLOG-APP/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Desktop/Blogs/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
